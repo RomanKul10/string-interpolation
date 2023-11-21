e2e3f682e0eb4d2359124ccfc426391b2db2f130
Roman Kulkov <romawka2410@gmail.com>
2023-11-21 12:24:11 +0200

    1

--- main.go
+++ main.go
@@ -4,17 +4,54 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
+var reader *bufio.Reader
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("What is your name")
+	reader = bufio.NewReader(os.Stdin)
+	userName := ReadString("What is your name?")
+	age := ReadInt("How old are you?")
+	//fmt.Println("Your name is "+userName+".You are", age, "years old.")
+	//fmt.Println(fmt.Sprintf("Your name is %s.You are %d years old.", userName, age))
+	fmt.Printf("Your name is %s. You are %d years old.", userName, age)
+}
+func prompt() {
 	fmt.Print("->")
+}
+func ReadString(s string) string {
+	for {
+		fmt.Println(s)
+		prompt()
 
-	userInput, _ := reader.ReadString('\n')
-	userInput = strings.Replace(userInput, "\r\n", "", -1)
-	userInput = strings.Replace(userInput, "\n", "", -1)
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+		if userInput == "" {
+			fmt.Println("Please enter value")
 
-	fmt.Println("Your name is", userInput)
+		} else {
+			return userInput
+		}
+	}
+}
+func ReadInt(s string) int {
+	for {
+		fmt.Println(s)
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.Atoi(userInput)
+		if err != nil {
+			fmt.Println("Please enter a whole number")
+		} else {
+			return num
+		}
+	}
+
 }
